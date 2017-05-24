bffcc60a9da9c36b526e85b8a70ded894e15afa0
John Michael Ferraris <johnmichaelferraris@gmail.com>
2017-05-24 14:32:44 +0800

    Updated namespacing

--- Sharepoint/Exception.php
+++ Sharepoint/Exception.php
@@ -1,12 +1,14 @@
 <?php
 /**
  * Own exception classes
- * 
+ *
  * @author Tuomas Angervuori <tuomas.angervuori@gmail.com>
  * @license http://opensource.org/licenses/LGPL-3.0 LGPL v3
  */
 
 namespace Sharepoint;
 
-class Exception extends \Exception { }
+class Exception extends \Exception
+{
+}
 
